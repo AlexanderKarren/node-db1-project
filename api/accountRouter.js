8a17cadef8e71316b6bc7a7a42d84230a1a74ba4
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-04-13 16:39:32 -0600

    put, delete endpoints

--- api/accountRouter.js
+++ api/accountRouter.js
@@ -16,7 +16,7 @@ router.get('/:id', validateAccountId, (req, res) => {
 
 router.post('/', (req, res) => {
     db('accounts').insert(req.body).then(response => {
-        db('accounts').where('id', response[0]).then(response => res.status(200).json({ data: response }))
+        db('accounts').where('id', response[0]).then(response => res.status(201).json({ data: response }))
         .catch(error => res.status(500).json({ error: error.message }));
     })
     .catch(error => res.status(500).json({ error: error.message }));
@@ -24,7 +24,15 @@ router.post('/', (req, res) => {
 
 router.put('/:id', validateAccountId, (req, res) => {
     db('accounts').where('id', req.params.id).update(req.body).then(response => {
-        db('accounts').where('id', req.params.id).then(response => res.status(200).json({ data: response }))
+        db('accounts').where('id', req.params.id).then(response => res.status(201).json({ data: response }))
+        .catch(error => res.status(500).json({ error: error.message }));
+    })
+    .catch(error => res.status(500).json({ error: error.message }));
+})
+
+router.delete('/:id', validateAccountId, (req, res) => {
+    db('accounts').where('id', req.params.id).then(findRes => {
+        db('accounts').where('id', req.params.id).del().then(delRes => res.status(200).json({ data: findRes[0] }))
         .catch(error => res.status(500).json({ error: error.message }));
     })
     .catch(error => res.status(500).json({ error: error.message }));
